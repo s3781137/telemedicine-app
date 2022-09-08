c066f996b4d9f5acd088268ab706f0c435c20f1a
“Oliver <s3897734@student.rmit.edu.au>
2022-09-08 19:26:47 +1000

    Delete Everything

--- app/application/src/main/java/com/example/application/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.application;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-    User findByEmailAndPassword(String email, String password);
-
-}
