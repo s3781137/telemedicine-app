63a16cd7da7c6869b7b70b049dc2091765ee2a60
runhuizhou <s3865846@student.rmit.edu.au>
2022-09-12 16:20:50 +1000

    Resolved conflicts

--- app/flutter/flutter_application/lib/main.dart
+++ app/flutter/flutter_application/lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+import 'signIn.dart';
+import 'welcome.dart';
 
+void main() {
+  runApp(MaterialApp(
+    title: 'My App',
+    home: SignInApp(),
+    // Routes defined here
+    routes: {"/welcome": (context) => WelcomeScreen()},
+  ));
+}
