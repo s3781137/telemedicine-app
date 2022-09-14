a6437efc88dae8edcb0873969cfee4da4ab7eed8
runhuizhou <s3865846@student.rmit.edu.au>
2022-09-14 15:59:43 +1000

    Implemented Sign-up-chooser buttons

--- app/flutter/flutter_application/lib/signUp.dart
+++ app/flutter/flutter_application/lib/signUp.dart
@@ -24,14 +24,32 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: const Center(
+      body: Center(
         child: SizedBox(
           width: 600,
           child: Card(
-            child: Text("patient?doctor?"),
-          ),
+              child: Column(
+            children: [
+              patientSignUpButton(),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+              ),
+              doctorSignUpButton(),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+              ),
+            ],
+          )),
         ),
       ),
     );
+  }
+
+  Widget patientSignUpButton() {
+    return ElevatedButton(onPressed: () {}, child: Text('I am patient'));
+  }
+
+  Widget doctorSignUpButton() {
+    return ElevatedButton(onPressed: () {}, child: Text('I am doctor'));
   }
 }
