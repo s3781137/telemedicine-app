c814ce6442472a382ff7ca6ae999ed568a43853c
runhuizhou <s3865846@student.rmit.edu.au>
2022-09-15 13:09:42 +1000

    create listview for cancel appoinment page

---
+++ app/flutter/flutter_application/lib/screens/cancel_appointment.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CancelAppointmentScreen extends StatelessWidget {
+  const CancelAppointmentScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cancel An Appointment'),
+        centerTitle: true,
+      ),
+      body: _myListView(context),
+    );
+  }
+}
+
+Widget _myListView(BuildContext context) {
+  return ListView(
+    children: ListTile.divideTiles(
+      context: context,
+      tiles: [
+        ListTile(
+          title: Text('Dr Jeon Junkook, 09:30 26/08/2022, Manor Lakes'),
+        ),
+        ListTile(
+          title: Text('Dr Doja Cat, 10:30 26/08/2022, Footscray'),
+        ),
+        ListTile(
+          title: Text('Dr Justin Bieber, 11:30 16/01/2023, Melbourne Central'),
+        ),
+        ListTile(
+          title: Text('Dr Kanye West, 00:30 06/07/2023,Chadstone'),
+        )
+      ],
+    ).toList(),
+  );
+}
