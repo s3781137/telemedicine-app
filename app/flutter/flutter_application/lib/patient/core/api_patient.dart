3eff97e605f461d716e688620260154c4f3aeabc
runhuizhou <s3865846@student.rmit.edu.au>
2022-09-19 12:08:50 +1000

    Implemented get user data api in frontend

--- app/flutter/flutter_application/lib/patient/core/api_patient.dart
+++ app/flutter/flutter_application/lib/patient/core/api_patient.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 
 import '../model/patient_model.dart';
@@ -14,6 +16,18 @@ class ApiClient {
         return response.body.toString();
       }
     } catch (e) {}
+  }
+
+  Future<PatientModel> getUser(String username) async {
+    Response response = await http
+        .get(Uri.parse('http://localhost:8080/patient/getBy/$username'));
+    if (response.statusCode == 200) {
+      // todo: remove debug message
+      print(PatientModel.fromJson(jsonDecode((response.body))).email);
+      return PatientModel.fromJson(jsonDecode((response.body)));
+    } else {
+      throw Exception('Failed to load userdata');
+    }
   }
 
   Future<List<PatientModel>> fetchUsers() async {
