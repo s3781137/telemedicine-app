afe466bc2ec66395cca49cf487aa46778c86bc3e
runhuizhou <s3865846@student.rmit.edu.au>
2022-09-19 11:49:01 +1000

    Implemented fetchUsers api in frontend

--- app/flutter/flutter_application/lib/patient/core/api_patient.dart
+++ app/flutter/flutter_application/lib/patient/core/api_patient.dart
@@ -1,9 +1,12 @@
 import 'package:http/http.dart';
 
+import '../model/patient_model.dart';
+import 'package:http/http.dart' as http;
+
 class ApiClient {
   Future<dynamic> login(String username, password) async {
     try {
-      Response response = await post(
+      Response response = await http.post(
           Uri.parse('http://localhost:8080/patient/login'),
           body: {'username': username, 'password': password});
 
@@ -11,5 +14,17 @@ class ApiClient {
         return response.body.toString();
       }
     } catch (e) {}
+  }
+
+  Future<List<PatientModel>> fetchUsers() async {
+    final response =
+        await http.get(Uri.parse('http://localhost:8080/patient/list'));
+    if (response.statusCode == 200) {
+      print('response body ${response.body}');
+      // todo: remove debug msg
+      return patientModelFromJson(response.body);
+    } else {
+      throw Exception('Unable to fetch products from the REST API');
+    }
   }
 }
