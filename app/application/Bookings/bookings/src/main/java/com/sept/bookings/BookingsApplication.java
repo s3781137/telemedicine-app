5f40fe415caddfb83d2039e164e404af8957d8b4
aisho26 <s3781589@student.rmit.edu.au>
2022-09-28 23:31:56 +1000

    //created a booking microservice

---
+++ app/application/Bookings/bookings/src/main/java/com/sept/bookings/BookingsApplication.java
@@ -0,0 +1,13 @@
+package com.sept.bookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingsApplication.class, args);
+	}
+
+}
---
+++ app/application/Bookings/bookings/src/test/java/com/sept/bookings/BookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sept.bookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
