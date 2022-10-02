5ff02b48a512bc4f2ce959d9b9482d4e47677f84
aisho26 <s3781589@student.rmit.edu.au>
2022-10-02 19:46:22 +1100

    //created an admin microservice

---
+++ app/application/Adminmicroservice/src/main/java/com/example/adminmicroservice/AdminmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.adminmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminmicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminmicroserviceApplication.class, args);
+    }
+
+}
