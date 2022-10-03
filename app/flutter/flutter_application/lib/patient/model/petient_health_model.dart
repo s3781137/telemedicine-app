d43f3260bf2c98e1e20472f20ad0367068e17c46
runhuizhou <s3865846@student.rmit.edu.au>
2022-10-04 00:20:50 +1100

    Create Patient Health Model

---
+++ app/flutter/flutter_application/lib/patient/model/petient_health_model.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+class PatientHealthModel {
+  PatientHealthModel(
+      {required this.id,
+      required this.cancer,
+      required this.diabetes,
+      required this.heartDisease,
+      required this.kidneyDisease,
+      required this.liverDisease,
+      required this.medicalProblems,
+      required this.medication,
+      required this.medicationDescription,
+      required this.pastSurgeries});
+
+  int? id;
+  String? cancer;
+  String? diabetes;
+  String? heartDisease;
+  String? kidneyDisease;
+  String? liverDisease;
+  String? medicalProblems;
+  String? medication;
+  String? medicationDescription;
+  String? pastSurgeries;
+
+  factory PatientHealthModel.fromJson(Map<String, dynamic> json) =>
+      PatientHealthModel(
+          id: json["id"],
+          cancer: json["cancer"],
+          diabetes: json["diabetes"],
+          heartDisease: json["heartDisease"],
+          kidneyDisease: json["kidneyDisease"],
+          liverDisease: json["liverDisease"],
+          medicalProblems: json["medicalProblems"],
+          medication: json["medication"],
+          medicationDescription: json["medicationDescription"],
+          pastSurgeries: json["pastSurgeries"]);
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "cancer": cancer,
+        "diabetes": diabetes,
+        "heartDisease": heartDisease,
+        "kidneyDisease": kidneyDisease,
+        "liverDisease": liverDisease,
+        "medicalProblems": medicalProblems,
+        "medication": medication,
+        "medicationDescription": medicationDescription,
+        "pastSurgeries": pastSurgeries
+      };
+}
