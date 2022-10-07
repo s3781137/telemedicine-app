513d57c7e06c930359acca1c574a5a44b1bcc039
s3781137 <s3781137@student.rmit.edu,au>
2022-10-07 20:56:11 +1100

    fixed port/docker issues

--- app/application/HealthInformation/healthinformation/src/test/java/com/sept/doctor/HealthInfoTests.java
+++ app/application/HealthInformation/healthinformation/src/test/java/com/sept/doctor/HealthInfoTests.java
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DoctorApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class HealthInfoTests {
 
 }
