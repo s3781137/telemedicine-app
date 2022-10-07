f62873b1c9cca5e47696f4ca6c8a227e0bf50a9b
aisho26 <s3781589@student.rmit.edu.au>
2022-10-07 15:05:52 +1100

    test

--- app/application/Chatmicroservice/src/main/java/com/example/chatmicroservice/api/ChatController.java
+++ app/application/Chatmicroservice/src/main/java/com/example/chatmicroservice/api/ChatController.java
@@ -2,61 +2,71 @@ package com.example.chatmicroservice.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.messaging.handler.annotation.MessageMapping;
+// import org.springframework.messaging.handler.annotation.Payload;
+// import org.springframework.messaging.simp.SimpMessagingTemplate;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.example.chatmicroservice.model.ChatMessage;
-import com.example.chatmicroservice.model.ChatNotification;
-import com.example.chatmicroservice.service.ChatMessageService;
-import com.example.chatmicroservice.service.ChatRoomService;
+// import com.example.chatmicroservice.model.ChatMessage;
+// import com.example.chatmicroservice.model.ChatNotification;
+// import com.example.chatmicroservice.service.ChatMessageService;
+// import com.example.chatmicroservice.service.ChatRoomService;
 
-@Controller
-public class ChatController {
+@RestController
+@RequestMapping("/chat")
+ public class ChatController {
 
-    @Autowired private SimpMessagingTemplate messagingTemplate;
-    @Autowired private ChatMessageService chatMessageService;
-    @Autowired private ChatRoomService chatRoomService;
+        @GetMapping("/get")
+        public String message(){
+                return "hello";
+        }
 
-    @PostMapping("/chat")
-    public void processMessage(@RequestBody ChatMessage chatMessage) {
-        var chatId = chatRoomService
-                .getChatId(chatMessage.getSenderId(), chatMessage.getRecipientId(), true);
-        chatMessage.setChatId(chatId.get());
+//     @Autowired private SimpMessagingTemplate messagingTemplate;
+//     @Autowired private ChatMessageService chatMessageService;
+//     @Autowired private ChatRoomService chatRoomService;
 
-        ChatMessage saved = chatMessageService.save(chatMessage);
-        messagingTemplate.convertAndSendToUser(
-                Integer.toString(chatMessage.getRecipientId()),"/queue/messages",
-                new ChatNotification(
-                        saved.getId(),
-                        saved.getSenderId(),
-                        saved.getSenderName()));
-    }
+//     @PostMapping("/chat")
+//     public void processMessage(@RequestBody ChatMessage chatMessage) {
+//         var chatId = chatRoomService
+//                 .getChatId(chatMessage.getSenderId(), chatMessage.getRecipientId(), true);
+//         chatMessage.setChatId(chatId.get());
 
-    @GetMapping("/messages/{senderId}/{recipientId}/count")
-    public ResponseEntity<Long> countNewMessages(
-            @PathVariable int senderId,
-            @PathVariable int recipientId) {
+//         ChatMessage saved = chatMessageService.save(chatMessage);
+//         messagingTemplate.convertAndSendToUser(
+//                 Integer.toString(chatMessage.getRecipientId()),"/queue/messages",
+//                 new ChatNotification(
+//                         saved.getId(),
+//                         saved.getSenderId(),
+//                         saved.getSenderName()));
+//     }
 
-        return ResponseEntity
-                .ok(chatMessageService.countNewMessages(senderId, recipientId));
-    }
 
-    @GetMapping("/messages/{senderId}/{recipientId}")
-    public ResponseEntity<?> findChatMessages ( @PathVariable int senderId,
-                                                @PathVariable int recipientId) {
-        return ResponseEntity
-                .ok(chatMessageService.findChatMessages(senderId, recipientId));
-    }
+//     @GetMapping("/messages/{senderId}/{recipientId}/count")
+//     public ResponseEntity<Long> countNewMessages(
+//             @PathVariable int senderId,
+//             @PathVariable int recipientId) {
 
-    @GetMapping("/messages/{id}")
-    public ResponseEntity<?> findMessage ( @PathVariable int id) {
-        return ResponseEntity
-                .ok(chatMessageService.findById(id));
-    }
-}
+//         return ResponseEntity
+//                 .ok(chatMessageService.countNewMessages(senderId, recipientId));
+//     }
+
+//     @GetMapping("/messages/{senderId}/{recipientId}")
+//     public ResponseEntity<?> findChatMessages ( @PathVariable int senderId,
+//                                                 @PathVariable int recipientId) {
+//         return ResponseEntity
+//                 .ok(chatMessageService.findChatMessages(senderId, recipientId));
+//     }
+
+//     @GetMapping("/messages/{id}")
+//     public ResponseEntity<?> findMessage ( @PathVariable int id) {
+//         return ResponseEntity
+//                 .ok(chatMessageService.findById(id));
+//     }
+ }
