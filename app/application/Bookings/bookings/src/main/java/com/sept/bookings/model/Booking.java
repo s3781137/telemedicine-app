00ceb06d18ba03fc38df0f1c824d2ee8940a250a
aisho26 <s3781589@student.rmit.edu.au>
2022-10-08 23:55:12 +1100

    //finished booking model

---
+++ app/application/Bookings/bookings/src/main/java/com/sept/bookings/model/Booking.java
@@ -0,0 +1,67 @@
+package com.sept.bookings.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String doctorUsername;
+    private String patientUsername;
+    private String doctorName;
+    private String patientName;
+    private String bDate;
+    private String startTime;
+    private String endTime;
+
+    public Booking(){
+
+    }
+
+    public Booking(String doctorUsername, String patientUsername, 
+    String doctorName, String patientName, String String bDate, 
+    String startTime, String endTime){
+        this.doctorUsername = doctorUsername;
+        this.patientUsername = patientUsername;
+        this.doctorName = doctorName;
+        this.patientName = patientName;
+        this.bDate = bDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getDoctorUsername(){
+        return doctorUsername;
+    }
+
+    public String getPatientUsername(){
+        return patientUsername;
+    }
+
+    public String getDoctorName(){
+        return doctorName;
+    }
+
+    public String getPatientName(){
+        return patientName;
+    }
+
+    public String getbDate(){
+        return bDate;
+    }
+
+    public String getStartTime(){
+        return startTime;
+    }
+
+    public String getEndTime(){
+        return endTime;
+    }
+
+}
