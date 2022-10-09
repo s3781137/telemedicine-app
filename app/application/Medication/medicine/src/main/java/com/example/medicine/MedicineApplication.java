acc3f4cae92e8d0c7e08fa3b4e1992bfd3a3734e
aisho26 <s3781589@student.rmit.edu.au>
2022-10-09 19:34:30 +1100

    created a medicine microservice

---
+++ app/application/Medication/medicine/src/main/java/com/example/medicine/MedicineApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineApplication.class, args);
+	}
+
+}
---
+++ app/application/Medication/medicine/src/test/java/com/example/medicine/MedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
