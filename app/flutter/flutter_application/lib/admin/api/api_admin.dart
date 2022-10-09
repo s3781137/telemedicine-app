748303a77dece53abc2aff8f6a487be12af46f3e
runhuizhou <s3865846@student.rmit.edu.au>
2022-10-10 04:20:53 +1100

    Add signup method for admin api

---
+++ app/flutter/flutter_application/lib/admin/api/api_admin.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter_application/admin/model/admin_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiAdmin {
+  // Future<String?> logIn(String username, String password) async {
+  //   var res = await http.post(Uri.parse("http://localhost:8083/admin/login"),
+  //       body: {"username": username, "password": password});
+  //   if (res.statusCode == 200) return res.body;
+  //   return null;
+  // }
+
+  Future<int> signUp(AdminModel admin) async {
+    // todo debug msg
+    print("im registering");
+    var res = await http.post(
+      Uri.parse("http://localhost:8083/admin/createAdmin"),
+      body: jsonEncode(admin),
+      headers: <String, String>{"Content-Type": "application/json"},
+    );
+    print(res.toString());
+    return res.statusCode;
+  }
+}
