ee5b75a48a47a4ef5312314bf142624f554f7690
Oliver <s3897734@student.rmit.edu.au>
2022-10-13 02:11:22 +1100

    Added get chats in dart api

--- app/flutter/flutter_application/lib/doctor/core/api_chat.dart
+++ app/flutter/flutter_application/lib/doctor/core/api_chat.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:flutter_application/doctor/model/chatModel.dart';
 import 'package:http/http.dart';
 
@@ -5,13 +8,25 @@ import 'package:http/http.dart' as http;
 
 class chatApiClient {
   // Return an instance of PatientModel
-  Future<chatModel> getChats(String sender, String recipient) async {
-    Response response =
-        await http.get(Uri.parse('http://localhost:8089/chat/'));
+  Future<chatModel> getChats(Int senderID, Int recipientID) async {
+    Response response = await http.get(Uri.parse(
+        'http://localhost:8089/chat/findMessages?senderId=${senderID}&recipientId=${recipientID}'));
     if (response.statusCode == 200) {
-      return PatientModel.fromJson(jsonDecode((response.body)));
+      return chatModel.fromJson(response.body);
     } else {
-      throw Exception('Failed to load userdata');
+      throw Exception('Failed to load chats');
     }
   }
+  /*
+  Future<chatModel> postChat(Int senderID, Int recipientID, String senderName,
+      String recipientName) async {
+    Response response = await http.get(Uri.parse(
+        'http://localhost:8089/chat/findMessages?senderId=${senderID}&recipientId=${recipientID}'));
+    if (response.statusCode == 200) {
+      return chatModel.fromJson(response.body);
+    } else {
+      throw Exception('Failed to load chats');
+    }
+  }
+  */
 }
