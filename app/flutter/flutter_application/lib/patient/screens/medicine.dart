ee703f3235bcbe296d6b6f92bba15998ed49f5b2
runhuizhou <s3865846@student.rmit.edu.au>
2022-10-13 13:43:45 +1100

    Make medicine(prescription screen)

---
+++ app/flutter/flutter_application/lib/patient/screens/medicine.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/main.dart';
+import 'package:flutter_application/patient/model/patient_booking_model.dart';
+
+import '../core/api_patient.dart';
+import '../model/patient_medicine_model.dart';
+import '../patient.dart';
+import '../widgets/appbar.dart';
+
+class ViewMedicineScreen extends StatefulWidget {
+  const ViewMedicineScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ViewMedicineScreen> createState() => _ViewMedicineScreenState();
+}
+
+class _ViewMedicineScreenState extends State<ViewMedicineScreen> {
+  final ApiClient _apiClient = ApiClient();
+  int? id = -1;
+  List<PatientMedicineModel> _medicines = [];
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+
+  void _load() async {
+    // todo: debug msg
+    print(
+        "_load async: current user: ${currentLoggedInUser["username"].toString()}");
+    List<PatientMedicineModel> medicines = await _apiClient.fetchMedicines(
+        currentLoggedInUser["username"]
+            .toString()); // load the availabilities on Widget init
+
+    setState(() => _medicines = medicines);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: makeAppBar(context),
+      body: ListView.builder(
+        itemCount: _medicines.length,
+        itemBuilder: (BuildContext ctxt, int i) {
+          return GestureDetector(
+            // todo on tap
+            onTap: () => {},
+            child: Column(
+              children: [
+                Text("Doctor: ${_medicines[i].doctorUsername} "),
+                Text("Date and Time: ${_medicines[i].medicine}")
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
