d2071ec268159e011e6f0ed782e4be0c278bcbb5
runhuizhou <s3865846@student.rmit.edu.au>
2022-10-13 11:21:57 +1100

    Connect viewing appoinment page to backend

--- app/flutter/flutter_application/lib/patient/screens/view_appointment.dart
+++ app/flutter/flutter_application/lib/patient/screens/view_appointment.dart
@@ -1,38 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/main.dart';
+import 'package:flutter_application/patient/model/patient_booking_model.dart';
 
-class ViewAppointmentScreen extends StatelessWidget {
-  const ViewAppointmentScreen();
+import '../core/api_patient.dart';
+import '../patient.dart';
+
+class ViewBookingScreen extends StatefulWidget {
+  const ViewBookingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ViewBookingScreen> createState() => _ViewBookingScreenState();
+}
+
+class _ViewBookingScreenState extends State<ViewBookingScreen> {
+  final ApiClient _apiClient = ApiClient();
+  int? id = -1;
+  List<PatientBookingModel> _bookings = [];
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+
+  void _load() async {
+    // todo: debug msg
+    print(
+        "_load async: current user: ${currentLoggedInUser["username"].toString()}");
+    List<PatientBookingModel> bookings = await _apiClient.fetchBookings(
+        currentLoggedInUser["username"]
+            .toString()); // load the availabilities on Widget init
+
+    setState(() => _bookings = bookings);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('View Current Bookings'),
-        centerTitle: true,
+        title: Text('ND TELEMEDICINE'),
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: PopupMenuButton(
+                child: Icon(Icons.account_circle),
+                itemBuilder: (context) {
+                  return [
+                    const PopupMenuItem(
+                      value: 'logout',
+                      child: Text('Log out'),
+                    ),
+                    const PopupMenuItem(
+                      value: 'changePW',
+                      child: Text('Change Password'),
+                    ),
+                    const PopupMenuItem(
+                      value: 'rmAccount',
+                      child: Text('Delete account'),
+                    ),
+                  ];
+                },
+              )),
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Patient()));
+                },
+                child: Icon(Icons.home),
+              )),
+        ],
       ),
-      body: _myListView(context),
+      body: ListView.builder(
+        itemCount: _bookings.length,
+        itemBuilder: (BuildContext ctxt, int i) {
+          return GestureDetector(
+            // todo on tap
+            onTap: () => {},
+            child: Column(
+              children: [
+                Text("Doctor: ${_bookings[i].doctorUsername} "),
+                Text("Date and Time: ${_bookings[i].dateTime}")
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
-}
-
-Widget _myListView(BuildContext context) {
-  return ListView(
-    children: ListTile.divideTiles(
-      context: context,
-      tiles: [
-        ListTile(
-          title: Text('Dr Jeon Junkook, 09:30 26/08/2022, Manor Lakes'),
-        ),
-        ListTile(
-          title: Text('Dr Doja Cat, 10:30 26/08/2022, Footscray'),
-        ),
-        ListTile(
-          title: Text('Dr Justin Bieber, 11:30 16/01/2023, Melbourne Central'),
-        ),
-        ListTile(
-          title: Text('Dr Kanye West, 00:30 06/07/2023,Chadstone'),
-        )
-      ],
-    ).toList(),
-  );
 }
