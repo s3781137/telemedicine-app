4d674f129a523c8dc7c238d2c29025964738fe7b
aisho26 <s3781589@student.rmit.edu.au>
2022-10-18 18:32:18 +1100

    finished junit tests for chat

--- app/application/Chat/chatmicroservice/src/test/java/com/example/chatmicroservice/ChatControllerTests.java
+++ app/application/Chat/chatmicroservice/src/test/java/com/example/chatmicroservice/ChatControllerTests.java
@@ -29,5 +29,39 @@ public class ChatControllerTests {
         ChatMessage cm = new ChatMessage(78, 67, 67, "Aishwarya", "Jimin", "park jimin", MessageStatus.RECEIVED);
         Assertions.assertNotNull(chatMessageService.save(cm));
     }
+
+    @Test
+    public void testListMessages() throws RuntimeException{
+        ChatMessage cm = new ChatMessage(30, 36, 39, "Aishwarya", "Jimin", "park jimin", MessageStatus.RECEIVED);
+        chatMessageService.save(cm);
+        Assertions.assertNotNull(chatMessageService.findAll());
+    }
+
+    @Test
+    public void testCountMessage() throws RuntimeException{
+        ChatMessage cm = new ChatMessage(102, 103, 104, "Jungkook", "Jimin", "yo", MessageStatus.RECEIVED);
+        chatMessageService.save(cm);
+        Assertions.assertEquals(chatMessageService.countNewMessages(103, 104, MessageStatus.RECEIVED), 1);
+    }
+
+    @Test
+    public void testFindMessage() throws RuntimeException{
+        ChatMessage cm = new ChatMessage(77, 78, 79, "rm", "Jimin", "hello", MessageStatus.RECEIVED);
+        chatMessageService.save(cm);
+        
+        Assertions.assertNotNull((chatMessageService.findChatMessages(78, 79)));
+
+    }
+
+    @Test
+    public void testFindById() throws RuntimeException{
+        ChatMessage cm = new ChatMessage(55, 71, 72, "min", "jin", "hi", MessageStatus.RECEIVED);
+        chatMessageService.save(cm);
+        int id =chatMessageService.findChatMessages(71, 72).get(0).getId();
+        Assertions.assertNotNull(chatMessageService.findById(id));
+
+    }
+
+
     
 }
