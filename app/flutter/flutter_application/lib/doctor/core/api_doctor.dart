bd24efd57aa5ef01a4e27c1527804e250d3980bd
runhuizhou <s3865846@student.rmit.edu.au>
2022-10-18 15:26:22 +1100

    doctor: Add option to view health info

--- app/flutter/flutter_application/lib/doctor/core/api_doctor.dart
+++ app/flutter/flutter_application/lib/doctor/core/api_doctor.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
 
 class ApiDoctor {
   Future<String?> logIn(String username, String password) async {
